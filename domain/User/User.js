ea8dc07749db1ab0de79f0b6d46163bb1be89898
Jonas Duri <jonas.duri@gmail.com>
2021-07-24 21:34:01 +0200

    format all js files with Prettier

--- domain/User/User.js
+++ domain/User/User.js
@@ -1,55 +1,59 @@
-import * as uuid from 'https://cdn.skypack.dev/pin/uuid@v8.3.2-XAQ9X05vJqvKpZccxFGc/mode=imports,min/optimized/uuid.js';
+import * as uuid from "https://cdn.skypack.dev/pin/uuid@v8.3.2-XAQ9X05vJqvKpZccxFGc/mode=imports,min/optimized/uuid.js";
 
 function validateEmail(email) {
-    const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const res = reg.exec(String(email).toLowerCase())
-    return res != null
-  }
+  const reg =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const res = reg.exec(String(email).toLowerCase());
+  return res != null;
+}
 
 export default class User {
-    static validate(name, email) {
-      if(typeof name != "string" || name === "") {
-        throw new Error("invariant error")
-      }
-      if(typeof email != "string" || email === "") {
-        throw new Error("invariant error")
-      }
-      if(!validateEmail(email)) {
-        throw new Error("invalid email error")
-      }
+  static validate(name, email) {
+    if (typeof name != "string" || name === "") {
+      throw new Error("invariant error");
     }
-
-    constructor(name, email) {
-      User.validate(name, email)
-      this._name = name
-      this._email = email
-      this._id = uuid.v4()
-      this._emailDomain = this._email.split("@")[1]
-      this._emailUsername = this._email.split("@")[0].split("+")[0]
-      this._emailSuffix = this._email.split("@")[0].split("+")[1]
+    if (typeof email != "string" || email === "") {
+      throw new Error("invariant error");
     }
-    
-    id() {
-        return this._id
-    }
-  
-    name() {
-      return this._name
-    }
-    
-    email() {
-      return this._email
-    }
-    
-    equals(anotherUser) {
-      return this._emailDomain == anotherUser._emailDomain && this._emailUsername === anotherUser._emailUsername
-    }
-    
-    serialize() {
-        return {
-          id: this.id(),  
-          name: this.name(),
-          email: this.email()
-        }
+    if (!validateEmail(email)) {
+      throw new Error("invalid email error");
     }
   }
+
+  constructor(name, email) {
+    User.validate(name, email);
+    this._name = name;
+    this._email = email;
+    this._id = uuid.v4();
+    this._emailDomain = this._email.split("@")[1];
+    this._emailUsername = this._email.split("@")[0].split("+")[0];
+    this._emailSuffix = this._email.split("@")[0].split("+")[1];
+  }
+
+  id() {
+    return this._id;
+  }
+
+  name() {
+    return this._name;
+  }
+
+  email() {
+    return this._email;
+  }
+
+  equals(anotherUser) {
+    return (
+      this._emailDomain == anotherUser._emailDomain &&
+      this._emailUsername === anotherUser._emailUsername
+    );
+  }
+
+  serialize() {
+    return {
+      id: this.id(),
+      name: this.name(),
+      email: this.email(),
+    };
+  }
+}
--- port-adapter/primary/ReactApplication.js
+++ port-adapter/primary/ReactApplication.js
@@ -1,40 +1,40 @@
 export function ReactApplication(svc) {
-    const html = htm.bind(React.createElement);
+  const html = htm.bind(React.createElement);
 
-const App = (props) => {
-  const [users, setUsers] = React.useState([])
-  const [error, setError] = React.useState()
-  const formRef = React.useRef()
-  const nameRef = React.useRef()
-  const emailRef = React.useRef()
-  
-  React.useEffect(() => {
+  const App = (props) => {
+    const [users, setUsers] = React.useState([]);
+    const [error, setError] = React.useState();
+    const formRef = React.useRef();
+    const nameRef = React.useRef();
+    const emailRef = React.useRef();
+
+    React.useEffect(() => {
       (async () => {
-        const usrs = await svc.all()
-        setUsers(usrs)
-      })()
-  }, [])
+        const usrs = await svc.all();
+        setUsers(usrs);
+      })();
+    }, []);
 
-  async function saveUser(e) {
-      e.preventDefault()
-      setError()
-      const name = nameRef.current.value
-      const email = emailRef.current.value
+    async function saveUser(e) {
+      e.preventDefault();
+      setError();
+      const name = nameRef.current.value;
+      const email = emailRef.current.value;
       try {
-        const user = svc.newUser(name, email)
-        await svc.save(user)
+        const user = svc.newUser(name, email);
+        await svc.save(user);
         setUsers((prev) => {
-            return [...prev, user]
-        })
-      } catch(e) {
-        setError(e)
+          return [...prev, user];
+        });
+      } catch (e) {
+        setError(e);
         window.setTimeout(() => {
-            setError(undefined)
-        }, 3000)
+          setError(undefined);
+        }, 3000);
       }
-  }
+    }
 
-  return html`
+    return html`
     <div className="container mx-auto px-4 max-w-xl">
       <form ref=${formRef} className="mb-4">
         <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">Name</label>
@@ -45,39 +45,49 @@ const App = (props) => {
         <button type="submit" onClick=${saveUser} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">save</button>
       </form>
 
-      ${ error ? html`
-        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-            <strong class="font-bold">Error: </strong>
-            <span class="block sm:inline">${error.message}</span>
-        </div>
-      ` : html``}
+      ${
+        error
+          ? html`
+              <div
+                class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                role="alert"
+              >
+                <strong class="font-bold">Error: </strong>
+                <span class="block sm:inline">${error.message}</span>
+              </div>
+            `
+          : html``
+      }
       
-      ${ users ? html`
-        <table className="table-auto">
-            <thead>
-                <tr>
+      ${
+        users
+          ? html`
+              <table className="table-auto">
+                <thead>
+                  <tr>
                     <th className="px-4 py-2 text-left">Name</th>
                     <th className="px-4 py-2 text-left">E-Mail</th>
                     <th className="px-4 py-2 text-left">ID</th>
-                </tr>
-            </thead>
-            <tbody>
-
-            ${users.map(u => {
-                return html`
-                    <tr>
+                  </tr>
+                </thead>
+                <tbody>
+                  ${users.map((u) => {
+                    return html`
+                      <tr>
                         <td className="border px-4 py-2">${u.name()}</td>
                         <td className="border px-4 py-2">${u.email()}</td>
                         <td className="border px-4 py-2 text-xs">${u.id()}</td>
-                    </tr>
-                `
-            })}
-            </tbody>
-        </table>
-      ` : html``}
+                      </tr>
+                    `;
+                  })}
+                </tbody>
+              </table>
+            `
+          : html``
+      }
     </div>
   `;
-}
-        
-ReactDOM.render(html`<${App} />`, document.querySelector("#react-root"))
+  };
+
+  ReactDOM.render(html`<${App} />`, document.querySelector("#react-root"));
 }
